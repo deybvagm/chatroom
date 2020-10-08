2e940b678b097a54150ee1f0c7d52f8836e4c35c
deyber <deyber.valencia@techandsolve.com>
2020-10-07 21:54:51 -0500

    Unit Test for extracting the required content from csv file

--- test.py
+++ test.py
@@ -1,12 +1,20 @@
 import unittest
-from utils import request_stock
+from utils import request_stock, build_message
+import csv
 
-class TestSum(unittest.TestCase):
+class TestApi(unittest.TestCase):
 
-    def test_sum(self):
+    def test_stock_api_response_with_aapl_stok_code(self):
         url = 'https://stooq.com/q/l/?s=STOCK_CODE&f=sd2t2ohlcv&h&e=csv'
         text = request_stock(url, stock_code='aapl.us')
         self.assertTrue(text.startswith('AAPL.US quote is $')), 'Should start with AAPL.US'
+
+    def test_content_extraction_from_csv_file_and_build_message(self):
+        with open('tests/aapl.us.csv') as csvfile:
+            file_reader = csv.reader(csvfile)
+            text = build_message(file_reader)
+            self.assertEqual('AAPL.US quote is $114.305 per share', text), 'Text should be AAPL.US ...'
+
 
 
 if __name__ == '__main__':
