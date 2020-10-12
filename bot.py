6a0ff27d8f1f25d8ceeb73ad492531e074d95543
deyber <deyber.valencia@techandsolve.com>
2020-10-11 19:44:03 -0500

    Logger re-configured for bot

--- bot.py
+++ bot.py
@@ -1,4 +1,8 @@
 from containers import Container
+import logging
+
+logging.basicConfig(level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     container = Container()
